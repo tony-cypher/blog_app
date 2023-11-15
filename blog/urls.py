1667efe83b09a38d4b97456578c580ef25ddaefa
Cipher <gangarinyuri@gmail.com>
2023-11-15 19:04:14 +0100

    2nd: Template tag

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,9 @@
-from django.conf import settings
-from django.conf.urls.static import static
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
+    path('about/', views.AboutPage.as_view(), name='about'),
+    path('blog_detail/', views.DetailPage.as_view(), name='detail'),
+    path('contact/', views.ContactPage.as_view(), name='contact'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,15 @@ from django.views import generic
 
 class HomePage(generic.TemplateView):
     template_name = 'index.html'
+
+
+class AboutPage(generic.TemplateView):
+    template_name = 'about.html'
+
+
+class DetailPage(generic.TemplateView):
+    template_name = 'blog_detail.html'
+
+
+class ContactPage(generic.TemplateView):
+    template_name = 'contact.html'
